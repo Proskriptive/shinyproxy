5d4637dfc5113e6cada7426be63caf0143b7b7fa
hitfuture <brett.r.taylor@icloud.com>
2017-05-02 13:02:34 -0600

    updated buildshinyproxy.sh

--- buildshinyproxy.sh
+++ buildshinyproxy.sh
@@ -1,4 +1,4 @@
 git pull
 mvn clean install
-sudo -i cp -v  ~/.m2/repository/eu/openanalytics/shinyproxy/0.8.7/*.jar /opt/shinyproxy
+sudo -i cp -v  ~/.m2/repository/eu/openanalytics/shinyproxy/0.9.0/*.jar /opt/shinyproxy
 sudo systemctl restart shinyproxy
