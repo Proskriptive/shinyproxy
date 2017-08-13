790e94a3e9f2c7c1f1bccb8fb1af5834a5133e50
Lee Bousfield <ljbousfield@gmail.com>
2017-08-12 22:41:41 -0600

    Add Travis support

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+
+keyfile="$(mktemp)"
+
+base64 -d > "$keyfile" <<< "$DEPLOY_BASE64_SSH_KEY"
+
+ssh "${!1}" -i "$keyfile" -o SendEnv=TRAVIS_COMMIT -o StrictHostKeyChecking=no <<'EOF'
+
+set -eu
+
+cd shinyproxy
+
+git fetch
+
+if [[ "$(git rev-parse HEAD)" != "$(git rev-parse "$TRAVIS_COMMIT")" ]] && git merge-base --is-ancestor "$TRAVIS_COMMIT" HEAD; then
+  echo "On older version than HEAD; exiting"
+  exit
+fi
+
+git reset --hard "$TRAVIS_COMMIT"
+
+./buildshinyproxy.sh
+
+EOF
+
+rm "$keyfile"
