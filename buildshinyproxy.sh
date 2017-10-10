a63375cb8648e14dee542d434a04123973b884ce
Lee Bousfield <ljbousfield@gmail.com>
2017-10-10 12:41:46 -0600

    Use mvn package instead of mvn install
    Results in a much more stable output

--- buildshinyproxy.sh
+++ buildshinyproxy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -e
-mvn clean install
-cd ~/.m2/repository/eu/openanalytics/shinyproxy
-# TODO find a more stable solution
-sudo cp -v "$(find . -mindepth 1 -maxdepth 1 -type d -printf '%f\n' | sort -V | tail -n 1)"/*.jar /opt/shinyproxy
+mvn clean package
+sudo cp -v target/*.jar /opt/shinyproxy
 sudo systemctl restart shinyproxy
