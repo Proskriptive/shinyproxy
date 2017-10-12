6172c1a756203e9506c0cfec6661321ec19a7a52
Lee Bousfield <ljbousfield@gmail.com>
2017-10-12 12:30:44 -0600

    Restart docker on deploy
    It seemed to be causing port mapping issues otherwise

--- buildshinyproxy.sh
+++ buildshinyproxy.sh
@@ -2,4 +2,6 @@
 set -e
 mvn clean package
 sudo cp -v target/shinyproxy*.jar /opt/shinyproxy/shinyproxy.jar
-sudo systemctl restart shinyproxy
+sudo systemctl stop shinyproxy
+sudo systemctl restart docker
+sudo systemctl start shinyproxy
