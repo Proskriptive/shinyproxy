33388be5c806e2fd2b1f251eed45ef2270b54644
Lee Bousfield <ljbousfield@gmail.com>
2017-11-09 18:33:12 -0700

    Attempt cleaner deploy

--- buildshinyproxy.sh
+++ buildshinyproxy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 mvn clean package
-sudo cp -v target/shinyproxy*.jar /opt/shinyproxy/shinyproxy.jar
 sudo systemctl stop shinyproxy
+sudo cp -v target/shinyproxy*.jar /opt/shinyproxy/shinyproxy.jar
 sudo systemctl restart docker
 sudo systemctl start shinyproxy
