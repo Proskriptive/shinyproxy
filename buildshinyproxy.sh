f9db1e619ecc1d3c7743ed6d9176bf795f1a2fa9
Lee Bousfield <ljbousfield@gmail.com>
2017-11-13 12:45:23 -0700

    Only restart docker if necessary

--- buildshinyproxy.sh
+++ buildshinyproxy.sh
@@ -3,5 +3,7 @@ set -e
 mvn clean package
 sudo systemctl stop shinyproxy
 sudo cp -v target/shinyproxy*.jar /opt/shinyproxy/shinyproxy.jar
-sudo systemctl restart docker
+if [ "$(sudo docker ps -q)" != "" ]; then
+    sudo systemctl restart docker
+fi
 sudo systemctl start shinyproxy
